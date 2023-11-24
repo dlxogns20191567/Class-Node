0b51de25d8f2209af2c38d940f3b20d76ff78334
dlxogns20191567 <77xogns@naver.com>
2023-11-24 11:33:19 +0900

    add sver.js

---
+++ http.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.listen(8080, function(){
+    console.log("포트 8080 으로 서버 대기중....")
+});
+app.get('/book', function(req,res){
+    res.send("도서 페이지 입니다")
+})
